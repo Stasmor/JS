bd78efa1ad75a48e74e246578a639aa3d9ed522a
Stanislav Morokov <stasmor@gmail.com>
2022-10-06 21:54:19 +0300

    Home Work 3

---
+++ hillel/hw3.js
@@ -0,0 +1,9 @@
+const fstVal = +prompt('Pls type fst. number',0);
+const secVal = +prompt('Pls type sec. number',0);
+
+//Користувач ввів 2 і 2:
+alert(`Користувач ввів ${fstVal} і ${secVal}:
+    sum=${fstVal+secVal}
+    diff=${fstVal-secVal}
+    mult=${fstVal*secVal}
+    div=${(fstVal/secVal)} `); 
