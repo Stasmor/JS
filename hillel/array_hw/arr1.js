1cfd870615d080007c498d7e88ab6bb2289e915a
Stanislav Morokov <stasmor@gmail.com>
2022-10-13 12:39:14 +0300

    hw10+11

---
+++ hillel/array_hw/arr1.js
@@ -0,0 +1,25 @@
+// Створити масив, довжину та елементи якого задає користувач.
+
+// Відсортувати масив за зростанням.
+
+// Видалити елементи з масиву з 2 по 4 (включно!).
+
+
+// У міру змін виводити вміст масиву на сторінку.
+
+let arrLenth = +prompt('Введіть довжину массиву', 0);
+let arrValues = [];
+
+for (let index = 0; index < arrLenth; index++) {
+
+    arrValues.push(+prompt(`Введіть елемент № ${index}`,0));
+    console.log(`After push #${index}: ${arrValues}`);
+}
+    arrValues.sort((a,b) => a-b );
+    console.log('After sort: ',arrValues);
+
+    arrValues.splice(1,3);
+    console.log('After splice(el2-4): ',arrValues);
+
+    document.getElementById('RES').textContent =  `Finaly arr: ${arrValues}`;
+ 
