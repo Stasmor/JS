718869bb377c89dc2ae78d4eaa99a676f311ab75
Stanislav Morokov <stasmor@gmail.com>
2022-10-17 21:44:38 +0300

    hw13_changes

--- hillel/array_hw/arrFuncRemEl.js
+++ hillel/array_hw/arrFuncRemEl.js
@@ -1,14 +1,13 @@
 // Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
-// const array = [1, 2, 3, 4, 5, 6, 7];
+ const array = [1, 2, 3, 4, 5, 6, 7];
 // removeElement(array, 5 );
 // console.log(array);
-// // Результат: [1, 2, 3, 4, 6, 7]
 
 function removeElement(arrIn, num4Del) {
-    if(typeof(num4Del) !== 'number' || isNaN(num4Del)) return;
+    if(typeof(num4Del) !== 'number' || isNaN(num4Del) || arrIn.indexOf(num4Del)===-1) return;
     arrIn.splice(arrIn.indexOf(num4Del),1);   
 }
 
-// removeElement(array,5);
-// console.log(array);
+ removeElement(array,91);
+ console.log(array);
