2f687cc44c0c4900c16735ca25a94dd516c8cccb
Stanislav Morokov <stasmor@gmail.com>
2022-10-22 10:00:28 +0300

    aftr_uderstending_task

--- hillel/locking/locking.js
+++ hillel/locking/locking.js
@@ -3,23 +3,19 @@
  Все це із замиканнями, наприклад: sum(3) = 3 sum(5) = 8 sum(20) = 28
 */
 
- function outsideF(startVar){
-    in_lock=startVar;
-    return function(newVar){
-        return startVar+newVar
-    }
+ function outsideF(){
+    let in_lock = 0;// замкнута змінна що замкнулася у (sumWithLock = outsideF())
+    return function(startVar){
+        in_lock += startVar;
+        return in_lock 
+    };
 
-}
+};
 //////////////////////checking
-console.log('------------inside_f with number-------------'); 
+sumWithLock = outsideF(); 
 
-sum100500n = outsideF(100500); //if f-st val. is number
+console.log('res+3=',sumWithLock(3));//number
+console.log('res+2=',sumWithLock(2));//number
+console.log('res+200=',sumWithLock(200));//number
+console.log('res+\"200\"=',sumWithLock('200'));//string
 
-console.log('res+1=',sum100500n(1)); //number
-console.log('res+\"1\"=',sum100500n('1')); //string
-console.log('------------inside_f with string -------------');
-
-sum100500s = outsideF('100500');//if f-st val. is string
-
-console.log('res+1=',sum100500s(1));//number
-console.log('res+\"1\"=',sum100500s('1'));//string
