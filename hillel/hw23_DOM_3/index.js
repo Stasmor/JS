74e53541af97e24eadcc49c986c08890be4b3cd7
Stanislav Morokov <stasmor@gmail.com>
2022-11-13 21:08:39 +0200

    modify with alt...

--- hillel/hw23_DOM_3/index.js
+++ hillel/hw23_DOM_3/index.js
@@ -4,11 +4,15 @@
 const imgTag = document.querySelector('img');
 const src = imgTag.getAttribute('src');
 
+
 const num = (Math.floor(Math.random() * 6)+1);
 const srcMod = src.slice(0, src.length-6)+ num +'.jpeg';
+const altMod = 'dog'.concat(num);
 
 imgTag.setAttribute('src',srcMod);
+imgTag.setAttribute('alt',altMod);
 //console.log('srcM=',srcMod);
+//console.log('altM=',altMod);
 
 
 
