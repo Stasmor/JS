a57fda4b93bb461b0aefd344c8edbf68a35abf2c
Stanislav Morokov <stasmor@gmail.com>
2022-12-30 22:32:31 +0200

    with questions...

--- hillel/hw33_ISTORE+history/index.js
+++ hillel/hw33_ISTORE+history/index.js
@@ -1,8 +1,30 @@
 import {products} from './products.js';
 
+// import {meth4Orders} from './meth4Orders.js';
+// console.warn('inside 0 index.js');
+// const tmp = new meth4Orders
+// const addingOrder = tmp.addingOrder
 
-let qntr=0;
+
+export const orderBase = {};
+
+
+
+
+ let qntr=0;
+// const order = {};
+
 const prodList = document.querySelector('#prodList');
+
+export function addingOrder(order){  //експортю для викор-ння по кнопці у product.js
+    const tmstamp = new Date();
+  
+     let orderNum = order.id +'_'+ String(tmstamp.valueOf()).slice(6,);// create unic ORDER ID
+    
+     orderBase[orderNum] = JSON.stringify(order,null,4)//pushing this order to object of orders
+    //  localStorage.setItem(orderNum,JSON.stringify(order,null,4));
+    console.log({orderBase});
+  }
 
 function addProd(model,valObj){
     const prodModelNew = model;
@@ -12,7 +34,16 @@ function addProd(model,valObj){
     prodModelNew.childNodes[1].insertAdjacentHTML('afterbegin',`<img src=${valObj.look} alt="${valObj.name}" width = 200 height = auto></img>`); //innerText = valObj.name;//image
     prodModelNew.childNodes[2].innerText = valObj.price+' $';
     if(!valObj.isActive) prodModelNew.childNodes[4].setAttribute('hidden',''); else prodModelNew.childNodes[4].removeAttribute('hidden');
-    prodModelNew.childNodes[4].addEventListener("click",()=>{redirect(valObj)}); 
+    prodModelNew.addEventListener("click",(ev)=>{
+        // console.log(ev.target.tagName);
+        if(ev.target.tagName === 'BUTTON' )  {
+            console.warn('this is a btn!!!');
+            addingOrder(valObj);
+        } else {
+            redirect(valObj)
+        }
+    }); 
+    // prodModelNew.childNodes[4].addEventListener("click",()=>{redirect(valObj)}); 
     prodList.appendChild(prodModelNew);
     qntr+=1;
 }
@@ -50,27 +81,43 @@ function addStruct(parent){
     mainDiv.appendChild(fragment);
     mainDiv.appendChild(btn);
 
+    console.warn('inside addStruct()');
     return parent.appendChild(mainDiv);
 }
+
+console.warn('inside index.js');
+showActiveProd();
 
 function redirect(item) { 
     history.pushState(item,'redirect','./product.html');
     history.go(0);  
+
+    //  location.assign('./product.html')
+    console.warn('after go!!!$$$$$');
+    console.warn('after go!!!$$$$$');
+    console.warn('after go!!!$$$$$');
+    console.warn('after go!!!$$$$$');
+    console.warn('after go!!!$$$$$');
+    console.warn('after go!!!$$$$$');
+    
 }
 
 function showActiveProd(){
     products.forEach((elem)=>{
+        console.log('before show actProd');
         addProd(addStruct(prodList), elem);
+        console.log('after show actProd');
         // if(elem.isActive) addProd2(prodModel,elem);
         // if(elem.isActive) addProd(prodModel,elem);
     })
 }
 
-showActiveProd();
 
 
-// third part of lesson task
-import MyCustomElement from './oop.js'
-const newObjEl = new MyCustomElement("_","div","MyCustomElement").addClass("products_new").addId("prodList_new").removeAttr("class")
-newObjEl.addAttr('type','text') 
-document.querySelector('.container').insertAdjacentElement('beforeend',newObjEl.newElem)//insert after all
+// // third part of lesson task
+// import MyCustomElement from './oop.js'
+// const newObjEl = new MyCustomElement("_","div","MyCustomElement").addClass("products_new").addId("prodList_new").removeAttr("class")
+// newObjEl.addAttr('type','text') 
+// document.querySelector('.container').insertAdjacentElement('beforeend',newObjEl.newElem)//insert after all
+
+// console.warn('***end.index.js');
--- hillel/hw33_ISTORE+history/product.js
+++ hillel/hw33_ISTORE+history/product.js
@@ -1,20 +1,38 @@
-window.addEventListener('popstate',()=>{ window.location.reload()});//forward/backwards go
+//import { addingOrder } from "./index.js";
+
+// import {products} from './products.js';
+// import {orderBase } from "./index.js";
+
+
+  // import {meth4Orders} from './meth4Orders.js';
+  // const tmp = new meth4Orders
+  // const addingOrder = tmp.addingOrder
+
+
+console.warn('inside product.js');
+
+ window.addEventListener('popstate',()=>{ window.location.reload()});//forward/backwards go+
 
   (function ins(){
     const sttObj={}
     Object.assign( sttObj ,history.state);
     const productSel = document.querySelector('.product');
+    console.warn('in1');
+    console.log('sttobj=',sttObj);
     shwProd(sttObj,productSel)
+    console.warn('in2');
   })();
 
-function addingOrder(order){
-  const tmstamp = new Date();
+//  function addingOrder(order){
+//   const tmstamp = new Date();
 
-   let orderNum = order.id +'_'+ String(tmstamp.valueOf()).slice(6,);// create unic ORDER ID
-   localStorage.setItem(orderNum,JSON.stringify(order,null,4));
-}
+//    let orderNum = order.id +'_'+ String(tmstamp.valueOf()).slice(6,);// create unic ORDER ID
+//    //localStorage.setItem(orderNum,JSON.stringify(order,null,4));
+//    orderBase[orderNum] = JSON.stringify(order,null,4)//pushing this order to object of orders
+// }
 ///////////////////// optionaly - localstorage //////////////////////////// 
 function showAllOrders(){
+  //  localStorage.clear();
    for (let index = 0; index < localStorage.length; index++) {
     console.log(JSON.parse(localStorage.getItem(localStorage.key(index))));
   }
@@ -26,16 +44,48 @@ function shwProd(obj,product){
     product.childNodes[1].innerText = obj.name;
     product.childNodes[3].insertAdjacentHTML('afterbegin',`<img src=${obj.look} alt="${obj.name}" width = 200 height = auto></img>`);
     product.childNodes[5].innerText = obj.price+' $';
-    product.childNodes[7].innerText = 'Buy';
-    product.childNodes[7].addEventListener("click",()=>{addingOrder(obj)}); 
+    product.childNodes[7].innerText = 'Add to cart';
+    product.childNodes[7].addEventListener("click",(ev)=>{
+        addingOrder(obj);
+        history.back();
+      }); 
+      console.warn('in3');
+
+
+    //   function addProd(model,valObj){
+    //     const prodModelNew = model;
+    //     //const prodModelNew = (qntr >= 1) ? model.cloneNode(true): model;//cloning
+    //     prodModelNew.setAttribute('data-id',valObj.id);
+    //     prodModelNew.childNodes[0].innerText = valObj.name;
+    //     prodModelNew.childNodes[1].insertAdjacentHTML('afterbegin',`<img src=${valObj.look} alt="${valObj.name}" width = 200 height = auto></img>`); //innerText = valObj.name;//image
+    //     prodModelNew.childNodes[2].innerText = valObj.price+' $';
+    //     if(!valObj.isActive) prodModelNew.childNodes[4].setAttribute('hidden',''); else prodModelNew.childNodes[4].removeAttribute('hidden');
+    //     prodModelNew.addEventListener("click",(ev)=>{
+    //         // console.log(ev.target.tagName);
+    //         if(ev.target.tagName === 'BUTTON' )  {
+    //             console.warn('this is a btn!!!');
+    //             addingOrder(valObj);
+    //         } else {
+    //             redirect(valObj)
+    //         }
+    //     }); 
+    //     // prodModelNew.childNodes[4].addEventListener("click",()=>{redirect(valObj)}); 
+    //     prodList.appendChild(prodModelNew);
+    //     qntr+=1;
+    // }
+
+
+
+
+
 
     ///////////////////// optionaly - localstorage ////////////////////////////  
-    const btn = document.createElement('button')
-    btn.textContent = 'showOrders';
-    btn.setAttribute("class", "product__btn");
-    product.appendChild(btn).addEventListener("click",()=>{showAllOrders()});
+    // const btn = document.createElement('button')
+    // btn.textContent = 'showOrders';
+    // btn.setAttribute("class", "product__btn");
+    // product.appendChild(btn).addEventListener("click",()=>{showAllOrders()});
     ///////////////////////////////////////////////////////////////////////////
-
+    console.warn('in4');
       
  }
  
